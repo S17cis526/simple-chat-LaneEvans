2bea1375029115f09df1f2b9af28d29157d0d5fa
LaneEvans <laneevans96@gmail.com>
2017-04-13 14:22:13 -0500

    4/13/17

---
+++ public/simple-chat.js
@@ -0,0 +1,26 @@
+var socket = io();
+
+socket.on('welcome', function(text) {
+  $('<li>').text(text).appendTo('#message-log');
+});
+
+socket.on('message', function(message) {
+  var li = $('<li>')
+        .css('color', message.color)
+        .appendTo('#message-log');
+      $('<strong>').text(message.user).appendTo(li) //did not finish
+  $('<li>').text(message.text)
+           .css('color', message.color)
+           .appendTo('#message-log');
+});
+
+$('#chat-send').on('click', function() {
+  var text = $('#chat-text').val();
+  socket.emit('message', text);
+  $('#chat=text').val('');
+});
+
+$('#color').on('change', function() {
+  var color = $(this).val();
+  socket.emit('color', color);
+});
